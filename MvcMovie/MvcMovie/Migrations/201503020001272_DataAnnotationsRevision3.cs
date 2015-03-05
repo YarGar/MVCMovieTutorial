bfa814dfb2a1d25cb1f9e74065381c07a2a419aa
YarGar - Yarin Garcia <ygarciaone @ yahoo.com>
2015-03-04 23:55:55 -0500

    Finished Tutorial

---
+++ MvcMovie/MvcMovie/Migrations/201503020001272_DataAnnotationsRevision3.cs
@@ -0,0 +1,16 @@
+namespace MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotationsRevision3 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
